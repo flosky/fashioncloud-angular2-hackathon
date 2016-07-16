43e50895273edc4cc9b36d712e2b5cc1418ed38f
Florian Klemt <florian.klemt@fashion.cloud>
2016-07-16 10:12:52 +0200

    feat(user): Added user route and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
+import { UserComponent } from './user';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent },
+  { path: 'user/:id', component:  UserComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
