a5bed946f73bee7057e485b09cdd6f9474463387
Florian Klemt <florian.klemt@fashion.cloud>
2016-07-16 13:46:43 +0200

    feat(user): used routerLink directive when creating link to post

--- src/app/user/post/post.component.ts
+++ src/app/user/post/post.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'user-post',
+  directives: [...ROUTER_DIRECTIVES],
   template: `
     <div class="panel panel-default">
         <div class="panel-heading">
-            <b><span style='cursor: pointer;' (click)='goToPost(post.id)'>{{post.title}}</span></b>
+            <b><a [routerLink]="['/post', post.id]">{{post.title}}</a></b>
         </div>
         <div class="panel-body">
             <div>{{post.body}}</div>
@@ -17,10 +18,4 @@ import { Router } from '@angular/router';
 export class UserPost {
 
     @Input() post: any;
-
-    constructor(private router: Router) {}
-
-    goToPost(postId) {
-        this.router.navigate(['/post', postId]);
-    }
 }
