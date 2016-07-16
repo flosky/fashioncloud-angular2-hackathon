fae6c7e54d4d03b8e052c6fe1e1f7c3aa63a4282
Bassem Ghazouani <bassem@Bassems-MBP.lan>
2016-07-16 11:33:58 +0200

    hackathon(app.routes.ts): add the post path and fix conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { PostComponent } from './post';
-
+import { UserComponent } from './user';
+import { AlbumsComponent } from './albums';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent},
-  { path: 'post/:id' , component: PostComponent}
+  { path: 'post/:id' , component: PostComponent},
+  { path: 'user/:id', component:  UserComponent },
+  { path: 'albums/:id', component: AlbumsComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
