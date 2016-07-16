c9e2c6edbe40e4f1292a533494ea2a7d427205a0
Bassem Ghazouani <bassem@Bassems-MBP.lan>
2016-07-16 11:29:01 +0200

    Hackathon(): adding the path for the post page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
+import { PostComponent } from './post';
+
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'post/:id' , component: PostComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
